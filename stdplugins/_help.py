9cc6d8f8cab6e9690001c03daaf286cf62226565
zeususerbot <50679858+zeususerbot@users.noreply.github.com>
2019-07-10 02:13:31 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -12,7 +12,7 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = "****:"
-    help_string = """@UniBorg ( **Custom Built By** @r4v4n4 ) \n**Verified Account**: ✅\n**Official Website**: https://ravanaisdrunk.site.live\n
+    help_string = """@UniBorg \n**Verified Account**: ✅\n\n
 Python {}
 Telethon {}
 
